e514ea1ca4db13bfdadc7ae9a716a9724cb19f24
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-09-17 05:09:03 +0530

    we can add logs to our application as like this. no need to add extra dependencies

--- src/main/java/com/learn/spring_cloud_functions/ToUpperCaseByClass.java
+++ src/main/java/com/learn/spring_cloud_functions/ToUpperCaseByClass.java
@@ -1,11 +1,15 @@
 package com.learn.spring_cloud_functions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.function.Function;
 
 //	Method 2 of implementing serverless functions
 
 public class ToUpperCaseByClass implements Function<String, String> {
 
+    Logger logger = LoggerFactory.getLogger(ToUpperCaseByClass.class);
     /**
      * This serverless function can be called as follows.
      * curl -H "Content-Type:text/plain" localhost:8083/toUpperCaseByClass -d "boooom"
@@ -14,6 +18,7 @@ public class ToUpperCaseByClass implements Function<String, String> {
      */
     @Override
     public String apply(String o) {
+        logger.info("apply method called. String received: {}", o);
         return String.valueOf(o).toUpperCase();
     }
 }
